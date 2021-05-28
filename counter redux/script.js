5be9cc23e698457d7826fadba3d31174d287ab32
Volta <giovoltadesign@gmail.com>
2021-05-27 21:16:02 -0400

    redux: counter

---
+++ counter redux/script.js
@@ -0,0 +1,52 @@
+// reducer
+const counter = (state = 0, action) => {
+	switch (action.type) {
+		case "INCREMENT":
+			return state + 1;
+		case "DECREMENT":
+			return state - 1;
+		default:
+			return state;
+	}
+};
+
+//  UI
+const Counter = ({ value, onIncrement, onDecrement }) => (
+	<div>
+		<h1>{value}</h1>
+		<button onClick={onIncrement}>+</button>
+		<button onClick={onDecrement}>-</button>
+	</div>
+);
+
+// call redux
+const { createStore } = Redux;
+
+// create store
+const store = createStore(counter);
+
+// method render react and dispatcher
+const render = () => {
+	ReactDOM.render(
+		<Counter
+			value={store.getState()}
+			onIncrement={() =>
+				store.dispatch({
+					type: "INCREMENT",
+				})
+			}
+			onDecrement={() =>
+				store.dispatch({
+					type: "DECREMENT",
+				})
+			}
+		/>,
+		document.getElementById("root")
+	);
+};
+
+// uddate state
+store.subscribe(render);
+
+// call function
+render();
